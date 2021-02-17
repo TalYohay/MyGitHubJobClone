4ff2782a9de88d255efdd90cbd9168af096cdd26
TalYohay <54473241+TalYohay@users.noreply.github.com>
2021-02-17 19:47:10 +0200

    Update positionservice.service.ts

--- src/app/services/positionservice.service.ts
+++ src/app/services/positionservice.service.ts
@@ -12,8 +12,8 @@ export class PositionserviceService {
 
 
   getAllPoistions2(page){
-    return this.http.get(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=${page}`);
-  }
+//     return this.http.get(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=${page}`);
+ return this.http.get(`https://cors.bridged.cc/https://jobs.github.com/positions.json?page=${page}`);  }
 
 
 
